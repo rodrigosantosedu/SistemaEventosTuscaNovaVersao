2fc84c47f1fca3438d1de2e96d38b7fea2bc92b2
Rodrigo Santos <rodrigobsi@usp.br>
2018-11-26 16:12:01 -0200

    Tela Inicial, Tela de cadastro, insercao e remocao funcionando

---
+++ src/sistematusca/SistemaTusca.java
@@ -0,0 +1,9 @@
+package sistematusca;
+import InterfaceGrafica.Principal;
+public class SistemaTusca {
+    public static void main(String[] args) {
+        Principal frm = new Principal();
+        frm.setVisible(true);
+    }
+    
+}
---
+++ src/utilitarios/ConectaBanco.java
@@ -0,0 +1,47 @@
+package utilitarios;
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+public class ConectaBanco {
+    public Statement stm;//public para acessar em outras classes
+    public ResultSet rs;//public para acessar em outras classes
+    private final String driver = "oracle.jdbc.driver.OracleDriver";
+    private final String caminho = "jdbc:oracle:thin:@grad.icmc.usp.br:15215:orcl";
+    private final String usuario = "G7143510";
+    private final String senha = "G7143510";
+    public Connection conn;//public para acessar em outras classes
+    
+    public void conexao(){ // metodo responsavel por realizar a conexao com o banco oracle
+        try {
+            System.setProperty("jdbc.Drivers",driver);// seta a propriedade do driver de conexao
+            conn = DriverManager.getConnection(caminho,usuario,senha);// realiza conexao
+            //JOptionPane.showMessageDialog(null, "conectado com sucesso");
+        } catch (SQLException ex) { // caso de "ruim" na conexao entra aqui
+            JOptionPane.showMessageDialog(null, "erro de conexao!\n ");
+            Logger.getLogger(ConectaBanco.class.getName()).log(Level.SEVERE, null, ex);
+        }    
+    }
+    
+   public void executaSQL(String sql){
+        try {
+            stm = conn.createStatement(rs.TYPE_SCROLL_INSENSITIVE, rs.CONCUR_READ_ONLY);
+            rs =  stm.executeQuery(sql);  
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro no ExecutaSQL!\n "+e.getMessage());
+        }
+        
+}
+    
+    public void desconecta(){// esse metodo serve para desconectar o banco
+        try {
+            conn.close();
+            JOptionPane.showMessageDialog(null, "Desconectado com sucesso!\n ");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "erro ao fechar a conexao!\n ");
+        }
+    }
+    
+}
